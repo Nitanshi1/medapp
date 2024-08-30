a48fb6b78e623afec4991ddd5db0aeaa2a25ddbd
Nitanshi Agarwal <nitanshianshi@gmail.com>
2024-08-30 12:17:44 +0530

    Intial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import {MatButtonModule} from '@angular/material/button';
+import { HomeComponent } from './components/home/home.component';
+import { CategoriesComponent } from './components/manage/categories/categories.component';
+import { CategoryFormComponent } from './components/manage/category-form/category-form.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatButtonModule,HomeComponent,CategoriesComponent,CategoryFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/manage/category-form/category-form.component.ts
@@ -0,0 +1,58 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { CategoriesComponent } from '../categories/categories.component';
+import { CategoryService } from '../../../services/category.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-category-form',
+  standalone: true,
+  imports: [
+    FormsModule,
+    MatInputModule,
+    CommonModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatLabel,
+  ],
+  templateUrl: './category-form.component.html',
+  styleUrl: './category-form.component.css',
+})
+export class CategoryFormComponent {
+  name!: string;
+  categoryService = inject(CategoryService);
+  router = inject(Router);
+  route= inject(ActivatedRoute);
+  isEdit=false;
+  id!:string;
+  ngOnInit() {
+   this.id = this.route.snapshot.params["id"];
+
+   if(this.id){
+    this.isEdit=true;
+    this.categoryService.getCategoryById(this.id).subscribe((result:any)=>{
+      console.log(result);
+      this.name=result.name;
+    });
+    
+  }
+}
+  add() {
+    console.log(this.name);
+    this.categoryService.addCategory(this.name).subscribe((result: any) => {
+      alert('Category added');
+      this.router.navigateByUrl('/admin/categories');
+    });
+  }
+  update(){
+    console.log(this.name);
+    this.categoryService.updateCategory(this.id,this.name).subscribe((result: any) => {
+      alert('Category updated');
+      this.router.navigateByUrl('/admin/categories');
+    });
+  }
+}
